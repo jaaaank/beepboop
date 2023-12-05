33231942928289e545123d6b4113c6b1153a0859
jamnk <pancakes3825@gmail.com>
2023-12-04 22:42:16 -0500

    spaghetti code :D

--- src/Scripts/BeepBullet.gd
+++ src/Scripts/BeepBullet.gd
@@ -10,9 +10,12 @@ func _physics_process(delta):
 
 
 func _on_Bullet_body_entered(body):
-	if body.get_collision_layer_bit(layer):
+	if body.get_collision_layer_bit(0):
 		body.call("damage")
-		print("hitbeep")
+		print("hitTurret")
+	if body.get_collision_layer_bit(2):
+		body.call("damage",10)
+		print("hitbeepboop")
 	queue_free()
 
 func _on_Bullet_area_entered(area):
