703dbcb2ff96545f839bcbcb53f4edf748073fe7
jamnk <pancakes3825@gmail.com>
2023-12-10 22:21:34 -0500

    test to see if github is whats breaking it

--- src/Scripts/ModeSwitcher.gd
+++ src/Scripts/ModeSwitcher.gd
@@ -3,6 +3,10 @@ extends Area2D
 var canswitch: bool = false
 var player
 
+onready var point:= $point
+onready var animp:= $AnimationPlayer
+onready var text:= $ModeText
+
 func _input(_event):
 	if Input.is_action_just_pressed("1") and canswitch:
 		player.call("switchMode",1)
@@ -13,11 +17,11 @@ func _input(_event):
 
 func _on_ModeSwitcher_body_entered(body):
 	player = body
-	if player.respawnpoint != $point.global_position:
-		player.respawnpoint = $point.global_position
-		$AnimationPlayer.play("activate")
+	if player.respawnpoint != point.global_position:
+		player.respawnpoint = point.global_position
+		animp.play("activate")
 	canswitch = true
-	$ModeText.visible = true
+	text.visible = true
 
 func _on_ModeSwitcher_body_exited(_body):
 	canswitch = false
--- src/Scripts/gun.gd
+++ src/Scripts/gun.gd
@@ -4,7 +4,7 @@ export (PackedScene) var bullet
 onready var gun = $GunSprite
 var cooldown: bool = false
 
-func _process(delta):
+func _process(_delta):
 	if !gun.flip_v and int(abs(rotation_degrees))%360>90 and int(abs(rotation_degrees))%360<270:
 		gun.flip_v = true
 	if gun.flip_v and int(abs(rotation_degrees))%360<90 or int(abs(rotation_degrees))%360>270:
