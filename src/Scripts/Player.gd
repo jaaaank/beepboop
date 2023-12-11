af9ae7ad2bd04bb97fa3056c3073c7f06698b76b
jamnk <pancakes3825@gmail.com>
2023-12-11 16:29:48 -0500

    fixed thing sowwy aiko

--- src/Scripts/Player.gd
+++ src/Scripts/Player.gd
@@ -3,6 +3,9 @@ extends Actor
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.25
 export var respawnpoint: Vector2 = Vector2(0,0)
+export var runSprite: Texture
+export var gunSprite: Texture
+export var jumpSprite: Texture
 
 #0 = normal mode
 #1 = jump mode (moves a bit faster and jumps much higher)
