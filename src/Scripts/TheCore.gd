cd7c1c29c9a73ee1a9f1e041cf92908ff2733e00
jamnk <pancakes3825@gmail.com>
2024-04-05 15:46:42 -0400

    core like exists now

--- src/Scripts/TheCore.gd
+++ src/Scripts/TheCore.gd
@@ -1,7 +1,6 @@
 extends Actor
 
-
-
+var x: int = 60
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,6 +8,20 @@ func _ready():
 
 func damage(dmg):
 	health -= dmg
-	$CanvasLayer/ProgressBar.value = health/15
+	$CanvasLayer/ProgressBar.value = health/150*100
 	if health <= 0:
-		queue_free()
+		$CanvasLayer/ProgressBar.queue_free()
+		$FullTimer.start(60)
+		$SecTimer.start(1)
+		$CanvasLayer/Label.bbcode_text = "[center][color=#ff2d00]TIME LEFT UNTIL FACTORY SELF-DESTRUCT: 60[/color]"
+
+
+func _on_SecTimer_timeout():
+	$SecTimer.start(1)
+	x-=1
+	$CanvasLayer/Label.bbcode_text = "[center][color=#ff2d00]TIME LEFT UNTIL FACTORY SELF-DESTRUCT: "+String(x) +"[/color]"
+
+
+func _on_FullTimer_timeout():
+	#she loses
+	pass
